c42ede157d806b7c63b674b3ab64d25309a812ec
WojBor87 <w.borawski@cotynatostudio.pl>
2023-04-03 21:36:40 +0200

    Classic pong game. For 2 players - right (w,s) and left (up, down)

--- main.py
+++ main.py
@@ -1,16 +1,47 @@
-# This is a sample Python script.
+import time
+from turtle import Screen
+from paddle import Paddle
+from ball import Ball
+from points import Scoreboard
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("PONG")
+screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+score = Scoreboard()
+
+screen.listen()
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(l_paddle.go_down, "s")
+screen.onkey(l_paddle.go_up, "w")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+game_is_on = True
+while game_is_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
 
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    if ball.xcor() > 380:
+        ball.reset_position()
+        score.l_point()
+        score.update_score()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    if ball.xcor() < -380:
+        ball.reset_position()
+        score.r_point()
+        score.update_score()
+
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 330 or ball.distance(l_paddle) < 50 and ball.xcor() < -330:
+        ball.bounce_x()
+
+screen.exitonclick()
